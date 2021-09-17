920f6a4c1bdcaf9d8968177201672af64f6879ff
kb18k <jackexi@gmail.com>
2021-09-17 20:35:13 +0800

    Initial Commit

---
+++ Classic05/Classic05App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic05App.swift
+//  Classic05
+//
+//  Created by kai wen chen on 2021/9/17.
+//
+
+import SwiftUI
+
+@main
+struct Classic05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
